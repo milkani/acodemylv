515c83d2f2a768e0905675617152edc9ed419a38
nmilka <nmilka@eisgroup.com>
2022-11-03 11:46:26 +0200

    [1_homework]: Add homework description

---
+++ src/main/java/lv/acodemy/homework/Variables.java
@@ -0,0 +1,16 @@
+package lv.acodemy.homework;
+
+public class Variables {
+    public static void main(String[] args) {
+
+        // Nr.1: Create 10 variables for each data type
+        // Nr.2: Using all Java Operators create arithmetic operations for each data type
+
+        // Nr.2: Example:
+
+        int firstNumber = 10;
+        int secondNumber = 20;
+        int result = firstNumber + secondNumber;
+        System.out.println(result);
+    }
+}
